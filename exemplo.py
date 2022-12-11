6afd75a4b41d121bcb85de45a8db98adf584c0b2
Natan Silva <natanlucas2012@gmail.com>
2022-12-11 12:44:46 -0300

    função já convertendo pra dataframe

---
+++ exemplo.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd 
+
+st.title('Meu primeiro app')
+
+st.markdown('''
+    ### Esse exemplo visa treinar o uso do streamlit para o projeto final de Aléssio ###
+
+    _testando o itálico_, **testando o negrito**
+''')
